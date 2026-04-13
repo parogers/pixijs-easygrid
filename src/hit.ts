5ce5ff5af2a836a3d2b31a6c61c5e87c929f38d7
Peter Rogers <peter.rogers@gmail.com>
2026-04-13 10:59:01 -0400

    fix: hit map generation should use alpha channel with threshold

--- src/hit.ts
+++ src/hit.ts
@@ -3,27 +3,34 @@
 export type HitMap = Map<string, boolean[][]>;
 
 
-export function getHitMapFromTexture(renderer, texture) {
+export type HitMapParams = {
+    // The opacity value at which the terrain is consdered to be solid
+    opacityThreshold?: number;
+}
+
+
+export function getHitMapFromTexture(renderer, texture, params: HitMapParams) {
+    const threshold = params.opacityThreshold ?? 1;
     const pixels = renderer.extract.pixels(texture); // rgba packed, row first
     const map = [];
     let pos = 0;
     for (let y = 0; y < pixels.height; y++) {
         map.push([]);
         for (let x = 0; x < pixels.width; x++) {
-            const a = pixels.pixels[pos];
-            pos += 4;
-            map[map.length-1].push(a > 0);
+            pos += 3; // skip over RGB
+            const a = pixels.pixels[pos++];
+            map[map.length-1].push(a >= threshold);
         }
     }
     return map;
 }
 
 
-export function getHitMapFromTileSheet(renderer, spritesheet) {
+export function getHitMapFromTileSheet(renderer, spritesheet, params: HitMapParams = {}) {
     return new Map(Object.keys(spritesheet.textures).map(name => {
         return [
             name,
-            getHitMapFromTexture(renderer, spritesheet.textures[name])
+            getHitMapFromTexture(renderer, spritesheet.textures[name], params)
         ];
     }));
 }
